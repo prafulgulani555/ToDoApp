2cc9e4158ab5809cc2cb495c550ba336e265cf49
prafulgulani555 <59774145+prafulgulani555@users.noreply.github.com>
2024-09-19 18:46:08 +0530

    implemented the complete and exit button

--- GUI.py
+++ GUI.py
@@ -1,17 +1,21 @@
 import FreeSimpleGUI as sg
 import functions
 
-label = sg.Text("Type in a ToDo")
+label = sg.Text("Enter a ToDo")
 input_box = sg.InputText(tooltip = "Enter ToDo", key = "todo")
 add_button = sg.Button("Add")
 
 list_box = sg.Listbox(values = functions.get_todos(), key = 'todos',
-                      enable_events = True, size = [45, 10])
+                      enable_events = True, size = (45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('ToDo App',
-                   layout = [[label], [input_box, add_button],
-                             [list_box, edit_button]],
+                   layout = [[label],
+                             [input_box, add_button],
+                             [list_box, edit_button, complete_button],
+                             [exit_button]],
                    font = ('Helvetica', 20))
 
 while True:
@@ -23,6 +27,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values = todos)
+            window['todo'].update(value = '')
 
         case "Edit":
             edit_todo = values['todos'][0]
@@ -33,8 +38,22 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values = todos)
+            window['todo'].update(value = '')
+
         case "todos":
             window['todo'].update(value = values['todos'][0])
+
+        case "Complete":
+            complete_todo = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(complete_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+            window['todo'].update(value = '')
+
+        case "Exit":
+            break
+
         case sg.WINDOW_CLOSED:
             break
 
